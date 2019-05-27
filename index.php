a0be8be7440af79ae71aadbe4d06312c51b31fac
arzakmiftakhul <miftakhularzak@gmail.com>
2019-05-28 01:02:56 +0700

    Update index.php

--- index.php
+++ index.php
@@ -172,7 +172,7 @@ if (isset($_POST['submit'])) {
 //     }
 // }
 
-?>
+
 
 
 <!-- <form method="post" action="phpQS.php?Cleanup&containerName=<?php echo $containerName; ?>">
@@ -279,3 +279,4 @@ Image to analyze:
 </div> -->
 </body>
 </html>
+?>
